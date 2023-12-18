c5069a23dc7e824218f3fdb1123d4587f83a6260
ihebmeftah <ihebmeftah666@gmail.com>
2023-12-18 09:56:00 +0100

    database and env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configServ: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configServ.get<string>('DB_HOST'),
+          port: configServ.get<number>('DB_PORT'),
+          username: configServ.get<string>('DB_USERNAME'),
+          password: configServ.get<string>('DB_PASSWORD'),
+          database: configServ.get<string>('DB_NAME'),
+          entities: ['dist/**/*.entity{.js,.ts}'],
+          migrations: ['dist/db/migrations/*{.js,.ts}'],
+          synchronize: configServ.get<boolean>('SYNC'),
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configser = app.get(ConfigService);
+  const port = configser.get<number>('APP_PORT');
+  await app.listen(port);
 }
 bootstrap();
